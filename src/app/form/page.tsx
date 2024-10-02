fb49e02cc22e6f9578a4d97a4878e061e6741189
taketori98 <takeuchi-yuto98@g.ecc.u-tokyo.ac.jp>
2024-10-03 00:54:32 +0900

    [update] Divide form into app/form and change directory structure

---
+++ src/app/form/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import "../style.css";
+import Button from "../components/button";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface FormInput {
+  name: string;
+}
+
+export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInput>();
+  const onSubmit: SubmitHandler<FormInput> = (data) =>
+    alert(JSON.stringify(data));
+  return (
+    <div>
+      <h2>入力フォーム</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="formcontent">
+          <label htmlFor="name">氏名: </label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Input your Name"
+            {...register("name", { required: "名前を入力してください" })}
+          />
+          {errors.name?.message && (
+            <p className="error-message">{errors.name?.message}</p>
+          )}
+        </div>
+        <Button height={30} width={100} buttonstyle="fill" type="submit">
+          ボタン
+        </Button>
+      </form>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,10 @@
 "use client";
-
-// import styles from "./page.module.css";
-import Button from "./components/button";
-import { useForm, SubmitHandler } from "react-hook-form";
-
-interface FormInput {
-  name: string;
-}
+import "./style.css";
 
 export default function Home() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormInput>();
-  const onSubmit: SubmitHandler<FormInput> = (data) =>
-    alert(JSON.stringify(data));
   return (
     <div>
-      <h2>Home</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="formcontent">
-          <label htmlFor="name">氏名: </label>
-          <input
-            type="text"
-            id="name"
-            placeholder="Input your Name"
-            {...register("name", { required: "名前を入力してください" })}
-          />
-          {errors.name?.message && (
-            <p className="error-message">{errors.name?.message}</p>
-          )}
-        </div>
-        <Button height={30} width={100} buttonstyle="fill" type="submit">
-          ボタン
-        </Button>
-      </form>
+      <h2>ホーム</h2>
     </div>
   );
 }
