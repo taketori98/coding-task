b0145d5536c3c0bcafdd66e9c5ebbd225cdbb38a
taketori98 <takeuchi-yuto98@g.ecc.u-tokyo.ac.jp>
2024-10-03 01:10:56 +0900

    [update] Update form element and add button in top page

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -6,6 +6,7 @@ import { useForm, SubmitHandler } from "react-hook-form";
 
 interface FormInput {
   name: string;
+  email: string;
 }
 
 export default function Form() {
@@ -18,22 +19,66 @@ export default function Form() {
     alert(JSON.stringify(data));
   return (
     <div>
-      <h2>入力フォーム</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <h2>会員登録</h2>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <div className="formcontent">
-          <label htmlFor="name">氏名: </label>
-          <input
-            type="text"
-            id="name"
-            placeholder="Input your Name"
-            {...register("name", { required: "名前を入力してください" })}
-          />
-          {errors.name?.message && (
+          <div>
+            <label htmlFor="name">氏名: </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="例）日本太郎"
+              {...register("name", { required: "名前を入力してください" })}
+            />
             <p className="error-message">{errors.name?.message}</p>
-          )}
+          </div>
+          <div>
+            <label htmlFor="agree">メールアドレス: </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="例）example@mail.com"
+              {...register("email", {
+                required: "メールアドレスは必須です。",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "有効なメールアドレスを入力してください。",
+                },
+              })}
+            />
+            <p className="error-message">{errors.email?.message}</p>
+          </div>
         </div>
-        <Button height={30} width={100} buttonstyle="fill" type="submit">
-          ボタン
+        <div>
+          <Button
+            height={30}
+            width={100}
+            buttonstyle="fill"
+            type="reset"
+            onClick={() => {
+              if (confirm("リセットしますか？")) {
+                const form = document.querySelector("form");
+                if (form) {
+                  form.reset();
+                  alert("リセットしました");
+                }
+              }
+            }}
+          >
+            リセット
+          </Button>
+          <Button height={30} width={100} buttonstyle="fill" type="submit">
+            登録する
+          </Button>
+        </div>
+        <Button
+          height={30}
+          width={100}
+          buttonstyle="fill"
+          type="button"
+          target="../"
+        >
+          トップに戻る
         </Button>
       </form>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 import "./style.css";
+import Button from "./components/button";
 
 export default function Home() {
   return (
     <div>
       <h2>ホーム</h2>
+      <Button height={30} width={100} buttonstyle="fill" target="/form">
+        登録する
+      </Button>
     </div>
   );
 }
